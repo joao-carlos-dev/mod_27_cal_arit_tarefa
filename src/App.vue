12e752603b380c9990a730a80b60a48edd372b29
João Carlos Borges <joaocarlosifsc@hotmail.com>
2024-02-13 09:58:16 -0300

    criando as const somar, subtrair, multiplicar, dividir e calculaResultado

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import { reactive } from 'vue';
 
 const estado = reactive({
   filtro: 'operacao',
-  NumeroTemp: '',
+  NumeroTemp1: '',
+  NumeroTemp2: '',
   operacao: [
     {
       valor: true,
@@ -12,49 +13,61 @@ const estado = reactive({
   ]
 })
 
-const getOperacaoSomar = () => {
-  return estado.tarefas.filtro(tarefa => tarefa.somar)
+const somar = (num1, num2) => {
+  return num1 + num2;
 }
 
-const getOperacaoDiminuir = () => {
-  return estado.tarefas.filtro(tarefa => tarefa.diminuir)
+const subtrair = (num1, num2) => {
+  return num1 - num2;
 }
 
-const getOperacaoMultiplicacao = () => {
-  return estado.tarefas.filtro(tarefa => tarefa.multiplicacao)
+const multiplicar = (num1, num2) => {
+  return num1 * num2;
 }
 
-const getOperacaoDivisao = () => {
-  return estado.tarefas.filtro(tarefa => tarefa.divisao)
-}
-
-const getOperacaoFiltradas = () => {
-  const { filtro } = estado;
-
-  switch (filtro) {
-    case 'somar':
-      return getOperacaoSomar();
-    case 'diminuir':
-      return getOperacaoDiminuir();
-    case 'multiplicacao':
-      return getOperacaoMultiplicacao();
-    case 'divisao':
-      return getOperacaoDivisao();
-    default:
-      return estado.tarefas;
+const dividir = (num1, num2) => {
+  if (num2 !== 0) {
+    return num1 / num2;
+  } else {
+    return "Divisão por zero não é permitido";
   }
+}
+
+const calcularResultado = () => {
+  const num1 = parseFloat(estado.NumeroTemp1);
+  const num2 = parseFloat(estado.NumeroTemp2);
+  let resultado = 0;
+
+  switch (estado.filtro) {
+    case 'somar':
+      resultado = somar(num1, num2);
+      break;
+    case 'diminuir':
+      resultado = subtrair(num1, num2);
+      break;
+    case 'multiplicacao':
+      resultado = multiplicar(num1, num2);
+      break;
+    case 'divisao':
+      resultado = dividir(num1, num2);
+      break;
+    default:
+      break;
+  }
+
+  estado.resultado = resultado;
 }
 
 const cadastraNumero01 = () => {
   const numeroNovo = {
-    valor: estado.NumeroTemp,
+    valor: estado.NumeroTemp1,
   }
   estado.tarefas.push(numeroNovo);
 }
 
 const cadastraNumero02 = () => {
   const numeroNovo = {
-    valor: estado.NumeroTemp,
+    valor: estado.NumeroTemp2,
   }
   estado.tarefas.push(numeroNovo);
 }
@@ -70,27 +83,27 @@ const cadastraNumero02 = () => {
 
     <div class="row">
       <div class="col-md-2">
-        <input required @change="evento => estado.NumeroTemp = evento.target.value" @submit="cadastraNumero01" type="number"
+        <input required @change="evento => estado.NumeroTemp1 = evento.target.value" @submit="cadastraNumero01" type="number"
           placeholder="Nº">
       </div>
       <div class="col-md-2">
-        <select class="ms-4" @change="evento => estado.filtro = evento.target.value">
+        <select class="ms-4" @change="calcularResultado">
           <option value="operações">Operações</option>
           <option value="somar">+</option>
-          <option value="diminuir">-</option>
+          <option value="subtrair">-</option>
           <option value="multiplicacao">*</option>
           <option value="divisao">/</option>
         </select>
       </div>
       <div class="col-md-2">
-        <input required @change="evento => estado.NumeroTemp = evento.target.value" @submit="cadastraNumero02" type="number"
+        <input required @change="evento => estado.NumeroTemp2 = evento.target.value" @submit="cadastraNumero02" type="number"
           placeholder="Nº">
       </div>
       <div class="col-md-1">
         <span class="ms-3">=</span>
       </div>
       <div class="col-md-2">
-        <input type="text" placeholder="R">
+        <input type="text" placeholder="Resultado">
       </div>
     </div>
   </div>
@@ -117,6 +130,11 @@ select {
   border-radius: 3px;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   background-color: rgba(94, 94, 243, 0.562);
+  cursor: pointer;
+}
+
+option {
+  text-align: center;
 }
 
 </style>
